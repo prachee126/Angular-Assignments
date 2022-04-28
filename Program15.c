6c2b7c48ad8d31f5fbb056568f4aa8bfc9ae0c18
Prachi Kavale <88667421+prachee126@users.noreply.github.com>
2022-04-28 12:45:42 +0530

    Add files via upload

---
+++ Program15.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int CountDigits(int iNo)
+{
+    int iCnt=0;
+    
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+    while (iNo>0)
+    {
+        iCnt++;
+        iNo=iNo/10;
+     }
+     return iCnt;
+}
+int main()
+{
+    int iValue=0,iRet=0;
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    iRet=CountDigits(iValue);
+    printf("Digits:\t%d",iRet);
+    return 0;
+}
